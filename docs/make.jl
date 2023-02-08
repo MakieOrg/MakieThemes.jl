2219c4ed14a36b5d42d5576ad79231c514aa84fc
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-02-08 12:19:47 +0530

    Add docs

---
+++ docs/make.jl
@@ -0,0 +1,56 @@
+using MakieThemes
+using Makie, CairoMakie, AlgebraOfGraphics, CSV
+using Documenter
+
+DocMeta.setdocmeta!(MakieThemes, :DocTestSetup, :(using MakieThemes, Makie, CairoMakie); recursive=true)
+
+# generate the GGThemr page
+open(joinpath(@__DIR__, "src", "themes", "ggthemr.jl"), "w") do io
+    println(io, "# GGThemr")
+
+    println(io, """
+    ```@setup ggthemr
+    using MakieThemes, CairoMakie, Makie
+    CairoMakie.activate!(type="svg")
+    ```
+    """)
+
+    for colortheme in MakieThemes.ggthemr_colorthemes
+        println(io, "## ggthemr(:$colortheme)")
+        println(io, """
+        ```@example ggthemr
+        MakieThemes.demofigure(ggthemr(:$colortheme))
+        ```
+        ```@example ggthemr
+        MakieThemes.demoscatter(ggthemr(:$colortheme))
+        ```
+        """)
+        println(io, "\n\n")
+    end
+end
+
+makedocs(;
+    modules=[MakieThemes],
+    authors="Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>, Anshul Singhvi <anshul.singhvi@columbia.edu>, and contributors",
+    repo="https://github.com/JuliaPlots/MakieThemes.jl/blob/{commit}{path}#{line}",
+    sitename="MakieThemes.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://juliaplots.github.io/MakieThemes.jl",
+        edit_link="master",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        "BBC" => "themes/bbc.md",
+        "GGThemr" => "themes/ggthemr.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/JuliaPlots/MakieThemes.jl",
+    devbranch="master",
+)
+
+# clean up
+rm(joinpath(@__DIR__, "src", "themes", "ggthemr.jl"))
---
+++ docs/src/themes/bbc.md
@@ -0,0 +1,11 @@
+# BBC
+
+The BBC theme is accessible through `theme_bbc()`.  You can get the BBC red color through `MakieThemes.BBC.bbc_red`.
+
+```@example env
+MakieThemes.demofigure(theme_bbc())
+```
+
+```@example env
+MakieThemes.demoscatter(theme_bbc())
+```
