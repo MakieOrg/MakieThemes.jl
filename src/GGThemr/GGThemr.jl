2894e21d949ad0409028d0b959b1ffbe4f2dfae1
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-02-08 11:55:45 +0530

    Fix strange corruption when using const in ggthemr

--- src/GGThemr/GGThemr.jl
+++ src/GGThemr/GGThemr.jl
@@ -8,52 +8,59 @@ export ggthemr, ggthemr_colorthemes
 ggthemr_colorthemes() = collect(keys(ColorTheme))
 
 
-const ggthemr_style = Attributes(
-    linewidth = 2,
+function _theme_ggthemr() 
+    return Attributes(
+        linewidth = 2,
 
-    fonts = (
-        regular = "Noto Sans",
-        bold = "Noto Sans Bold",
-        italic = "Noto Sans Italic",
-        bold_italic = "Noto Sans Bold Italic",
-    ),
+        fonts = (
+            regular = "Noto Sans",
+            bold = "Noto Sans Bold",
+            italic = "Noto Sans Italic",
+            bold_italic = "Noto Sans Bold Italic",
+        ),
 
-    Axis = Attributes(
-        spinewidth = 1.5,
-        bottomspinevisible = true,
-        topspinevisible = false,
-        leftspinevisible = true,
-        rightspinevisible = false,
-        xgridwidth = 2,
-        ygridwidth = 2,
-        xgridstyle = :dash,
-        ygridstyle = :dash,
-    ),
+        Axis = Attributes(
+            spinewidth = 1.5,
+            bottomspinevisible = true,
+            topspinevisible = false,
+            leftspinevisible = true,
+            rightspinevisible = false,
+            xgridwidth = 2,
+            ygridwidth = 2,
+            xgridstyle = :dash,
+            ygridstyle = :dash,
+        ),
 
-    Axis3 = Attributes(
-        xspinewidth = 1.5,
-        yspinewidth = 1.5,
-        zspinewidth = 1.5,
-
-    ),
-    Legend = Attributes(
-        bgcolor = :transparent,
-        framevisible = false,
-    ),
-    Scatter = Attributes(
-        markersize = 15,
-        strokewidth = 2,
-    ),
-
-)
+        Axis3 = Attributes(
+            xspinewidth = 1.5,
+            yspinewidth = 1.5,
+            zspinewidth = 1.5,
+            xspinestyle = :dash,
+            yspinestyle = :dash,
+            zspinestyle = :dash,
+            xgridstyle = :dash,
+            ygridstyle = :dash,
+            zgridstyle = :dash,
+        ),
+        Legend = Attributes(
+            bgcolor = :transparent,
+            framevisible = false,
+        ),
+        Scatter = Attributes(
+            markersize = 15,
+            strokewidth = 0,
+        ),
+    )
+end
 
 function ggthemr(theme::Symbol)
     ct = ColorTheme[theme]
     palettes = merge(Makie.default_palettes, Attributes(color = ct[:swatch][2:end]))
-    merge(ggthemr_style, Attributes(
+    merge(_theme_ggthemr(), Attributes(
         backgroundcolor = Makie.to_color(ct[:background]),
         palette = palettes,
         color = ct[:swatch][2], # maybe it should be ct[:swatch][1], this is the color to use in absence of grouping
+        textcolor = ct[:swatch][2],
         colormap = Makie.to_colormap(Makie.to_color.(ct[:gradient])),
         Axis = Attributes(
             backgroundcolor = :transparent,
@@ -69,6 +76,8 @@ function ggthemr(theme::Symbol)
             ylabelcolor = Makie.to_color(ct[:text][2]),
             xticklabelcolor = Makie.to_color(ct[:text][2]),
             yticklabelcolor = Makie.to_color(ct[:text][2]),
+            xtickcolor = Makie.to_color(ct[:gridline]),
+            ytickcolor = Makie.to_color(ct[:gridline]),
         ),
         Axis3 = Attributes(
             xspinecolor_1 = Makie.to_color(ct[:line][2]),
@@ -80,6 +89,12 @@ function ggthemr(theme::Symbol)
             zspinecolor_1 = Makie.to_color(ct[:line][2]),
             zspinecolor_2 = Makie.to_color(ct[:line][2]),
             zspinecolor_3 = Makie.to_color(ct[:line][2]),
+            xgridcolor = Makie.to_color(ct[:gridline]),
+            ygridcolor = Makie.to_color(ct[:gridline]),
+            zgridcolor = Makie.to_color(ct[:gridline]),
+            xtickcolor = Makie.to_color(ct[:gridline]),
+            ytickcolor = Makie.to_color(ct[:gridline]),
+            ztickcolor = Makie.to_color(ct[:gridline]),
         ),
         Legend = Attributes(
             framecolor = Makie.to_color(ct[:line][2]),
